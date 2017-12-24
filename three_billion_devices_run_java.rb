92d4970722d05f049a9606c8409c1fc9f3011af0
Akira Ouchi <akkiesoft@marokun.net>
2017-12-24 23:40:58 +0900

    気分で名前を変えた、specを.mikutter.ymlで置き換え

--- three_billion_devices_run_java.rb
+++ three_billion_devices_run_java.rb
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
-Plugin.create(:mikutter_3_billion_devices_run_java) do
+Plugin.create(:three_billion_devices_run_java) do
   java = _('3 Billion Devices Run Java')
   command(
-    :mikutter_3_billion_devices_run_java_cmd,
+    :three_billion_devices_run_java,
     name: java,
     condition: -> _ { true },
     visible: true,
     role: :timeline
   ) do
-    strs = java
-    Post.primary_service.update(:message => strs)
+    Post.primary_service.update(:message => java)
   end
 end
